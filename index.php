dbe3a04829e7948cd484a558e32b57a9355eb42c
Agustin Di Carlo <agus.dicarlo@gmail.com>
2025-01-14 19:46:34 -0300

    Estructura de carpetas

--- index.php
+++ index.php
@@ -3,3 +3,15 @@
     echo "hola mundo";
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sitio Web</title>
+</head>
+<body>
+    Sitio Web
+</body>
+</html>
