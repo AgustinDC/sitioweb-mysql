b80a73299f608f9fb2e403b0a1e59e7d9ca85894
Agustin Di Carlo <agus.dicarlo@gmail.com>
2025-01-17 07:20:30 -0300

    merge

--- index.php
+++ index.php
@@ -15,11 +15,17 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <ul class="nav navbar-nav">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Nav 1 <span class="sr-only">(current)</span></a>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Develoteca</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Nav 2</a>
+                <a class="nav-link" href="#">Inicio</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Libros</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Nosotros</a>
             </li>
         </ul>
     </nav>
