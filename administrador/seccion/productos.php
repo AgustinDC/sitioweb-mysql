a92fc21095edd419a9c76946e550ff0b6764febf
Agustin Di Carlo <agus.dicarlo@gmail.com>
2025-01-21 12:39:08 -0300

    conexion con db

--- administrador/seccion/productos.php
+++ administrador/seccion/productos.php
@@ -13,6 +13,20 @@
     echo $txtImagen . '<br/>';
     echo $accion . '<br/>';
 
+    $host="localhost";
+    $db="sitioweb";
+    $user="root";
+    $pass="";
+
+    try {
+        $connectionDb = new pdo("mysql:host=$host; dbname=$db" ,$user,$pass);
+        if($connectionDb){
+            echo 'Conectado a la BD';
+        }
+    } catch (Exception $ex ) {
+        echo $ex -> getMessage();
+    }
+
 
     switch($accion){
         case "agregar":
